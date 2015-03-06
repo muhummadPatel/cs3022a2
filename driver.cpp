ea5427d232ea6e291a74ae9b3848a6645eab1426
Muhummad Patel <muhummad.patel@gmail.com>
2015-03-06 13:54:57 +0200

    WIP: command line argument parser.

--- driver.cpp
+++ driver.cpp
@@ -1,7 +1,14 @@
+#include <iostream>
+
 #include "volImage.h"
 
-int main(void){
+int main(int argc, char* argv[]){
     using namespace ptlmuh006;
+    
+    for(int i = 1; i < argc; i++){
+        std::cout << argv[i] << " ";
+    }
+    std::cout << std::endl;
     
     VolImage vi;
     return 1;

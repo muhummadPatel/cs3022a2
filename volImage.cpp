d81204fbd1a5bcb2701a4332127d2cbbe98663d1
Muhummad Patel <muhummad.patel@gmail.com>
2015-03-07 17:04:39 +0200

    WIP: Change method of reading from raw files. Now reads one row at a time. Also add test method printSlice that outputs an ASCII representation of the slice to text file. According to the output, it seems to be reading in fine. (out.txt matches the given .png images for the slices)

--- volImage.cpp
+++ volImage.cpp
@@ -1,12 +1,44 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 #include "volImage.h"
 
 namespace ptlmuh006{
 
     using namespace std;
+    
+    void printSlice(unsigned char** slice, int width, int height){
+        for(int row = 0; row < height; row++){
+            for(int col = 0; col < width; col++){        
+                int val = (int)slice[row][col];
+                char intensity;
+                if (val < 28) {
+			        intensity = '.';
+		        } else if (val < 56) {
+			        intensity = ':';
+		        } else if (val < 85) {
+			        intensity = '-';
+		        } else if (val < 113) {
+			        intensity = '=';
+		        } else if (val < 141) {
+			        intensity = '+';
+		        } else if (val < 170) {
+			        intensity = '*';
+		        } else if (val < 198) {
+			        intensity = '#';
+		        } else if (val < 226) {
+			        intensity = '%';
+		        } else if (val < 255) {
+			        intensity = '@';
+		        }
+		        
+		        cout << intensity << " ";
+            }
+            cout << endl;
+        }
+    }
 
     VolImage::VolImage(){
         //cout << "VolImage()" << endl;
@@ -24,14 +56,10 @@ namespace ptlmuh006{
         //read dat file and get dimensions
         int numslices;
         ifstream hdr(baseName + ".dat");
-        hdr >> width;
-        hdr >> height;
-        hdr >> numslices;
-        /*
+        hdr >> width >> height >> numslices;
         cout << "w " << width << endl;
         cout << "h " << height << endl;
-        cout << "s " << slices << endl;
-        */
+        cout << "s " << numslices << endl;
         
         //for each slice
         //read into slice
@@ -46,30 +74,21 @@ namespace ptlmuh006{
             for(int row = 0; row < height; row++){
             
                 rowsArr[row] = new unsigned char [width];
+                slcfile.read((char*)rowsArr[row], (sizeof(unsigned char) * width));
+                /*
                 for(int col = 0; col < width; col++){
                     slcfile >> rowsArr[row][col];
                 }
+                */
             }
             slcfile.close();            
             slices.push_back(rowsArr);
         }
         
         cout << "loaded: " << slices.size() << endl;
+        printSlice(slices[4], width, height);
+        cout << sizeof(unsigned char) << endl;
         
-        /*
-        ifstream in(baseName, ios::binary);
-        
-        unsigned char item;
-        int c = 0;
-        while(in >> item){
-            c++;
-            cout << (int)item << " ";
-            if(c % 429 == 0){
-                cout << endl;
-            }
-        }
-        in.close();
-        */
         return true;
     }
 

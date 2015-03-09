d6a290aced98c4ea0edf642c4bac0032292132d3
Muhummad Patel <muhummad.patel@gmail.com>
2015-03-09 17:40:48 +0200

    WIP: Command line argument parser framework laid down. NOTE: compiles and runs aotc.

--- driver.cpp
+++ driver.cpp
@@ -1,26 +1,41 @@
 #include <iostream>
+#include <vector>
 
 #include "volImage.h"
 
 int main(int argc, char* argv[]){
     using namespace ptlmuh006;
     
-    {
+    //TODO: here be old codes
     VolImage vi;
     
-    std::string base = argv[1];
+    std::string base = "MRI";//argv[1];
     vi.readImages(base);
     
-    //vi.extract(4, "extracted");
+    vi.extract(4, "extracted");
     vi.diffmap(2, 3, "diffed");
-    }
     
-    /*
-    for(int i =0; i < 100; i++){
-        std::cout << "yo ";
+    
+    //new code
+    if(argc < 2){
+        //invalid usage
+        std::cout << "invalid usage" << std::endl;
+    }else if(argc == 2){
+        //just load data and exit
+        std::cout << "load data and exit" << std::endl;
+    }else{
+        std::cout << "handle flags" << std::endl;
+        
+        std::string flag = std::string(argv[2]);
+        if(flag == "-d"){
+            std::cout << "OPT diffmap" << std::endl;
+        }else if(flag == "-x"){
+            std::cout << "OPT extract" << std::endl;
+        }else{
+            //invalid usage
+            std::cout << "nvalid usage" << std::endl;
+        }
     }
-    std::cout << std::endl;
-    */
     
     return 0;
 }

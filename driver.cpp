e4561c57aeb6713b8fdc3faa1ec4855a2c1828b4
Muhummad Patel <muhummad.patel@gmail.com>
2015-03-11 16:51:15 +0200

    Improve argument parsing. Also add usage hint after invalid attempt to run the program.

--- driver.cpp
+++ driver.cpp
@@ -1,44 +1,62 @@
 #include <iostream>
-#include <vector>
+#include <sstream>
 
 #include "volImage.h"
+
+void handleInvalidArgs(){
+    using namespace std;
+    
+    ostringstream oss;
+    oss << "Usage: ";
+    oss << "./volimage <imageBase> ";
+    oss << "[-d sliceI sliceJ output_file_name] ";
+    oss << "[-x slice output_file_name] ";
+    oss << "[-g row output_file_name]";
+    
+    cout << oss.str() << endl;
+}
 
 int main(int argc, char* argv[]){
     using namespace ptlmuh006;
     
-    //TODO: here be old codes
     VolImage vi;
-    
-    std::string base = "MRI";//argv[1];
-    vi.readImages(base);
-    std::cout << "READ IMAGES" << std::endl;
-    //vi.extract(4, "extracted");
-    //vi.diffmap(2, 3, "diffed");
-    vi.extractRow(5, "rEXtr");
-    std::cout << "Row extract" << std::endl;
-
-    /*
-    //new code
     if(argc < 2){
-        //invalid usage
-        std::cout << "invalid usage" << std::endl;
+        handleInvalidArgs();
+        
     }else if(argc == 2){
         //just load data and exit
         std::cout << "load data and exit" << std::endl;
-    }else{
-        std::cout << "handle flags" << std::endl;
+        vi.readImages(argv[1]);
         
+    }else{
+        vi.readImages(argv[1]);
+        
+        //handle flags        
         std::string flag = std::string(argv[2]);
-        if(flag == "-d"){
-            std::cout << "OPT diffmap" << std::endl;
-        }else if(flag == "-x"){
-            std::cout << "OPT extract" << std::endl;
+        
+        if(flag == "-d" && argc == 6){
+            int sliceI = std::stoi(argv[3]);
+            int sliceJ = std::stoi(argv[4]);
+            std::string output_prefix = argv[5];
+            
+            vi.diffmap(sliceI, sliceJ, output_prefix);
+            
+        }else if(flag == "-x" && argc == 5){
+            int slice = std::stoi(argv[3]);
+            std::string output_prefix = argv[4];
+            
+            vi.extract(slice, output_prefix);
+            
+        }else if(flag == "-g" && argc == 5){
+            int row = std::stoi(argv[3]);
+            std::string output_prefix = argv[4];
+            
+            vi.extractRow(row, output_prefix);
+            
         }else{
-            //invalid usage
-            std::cout << "nvalid usage" << std::endl;
+            handleInvalidArgs();
         }
     }
-*/
     
     return 0;
 }

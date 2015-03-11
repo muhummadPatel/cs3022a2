ffaf2b1eb22cda9c65494fc4e5f96fb569c8ba73
Muhummad Patel <muhummad.patel@gmail.com>
2015-03-11 16:51:34 +0200

    Clean up code.

--- volImage.cpp
+++ volImage.cpp
@@ -43,33 +43,22 @@ namespace ptlmuh006{
     }
 
     VolImage::VolImage(){
-        //cout << "VolImage()" << endl;
         width = 0;
         height = 0;
     }
     
     VolImage::~VolImage(){
-        //cout << "~VolImage() "<< slices.size() << endl;
         for(int i = 0; i < slices.size(); i++){
-            //cout << "destruct slice " << i << endl;
             unsigned char ** rowsArr = slices[i]; 
             for(int row = 0; row < height; row++){
-                //cout << "del ";
                 delete [] rowsArr[row];
             }
-            
-            //cout << "del ";
             delete [] rowsArr;
         }
-       // cout << endl;
         slices.clear();
-        //cout << slices.size() << endl;
     }
     
     bool VolImage::readImages(std::string baseName){
-        //cout << "readImages(" << baseName << ")" << endl;
-        
-        //read dat file and get dimensions
         int numslices;
         ifstream hdr(baseName + ".dat");
         hdr >> width >> height >> numslices;
@@ -77,52 +66,33 @@ namespace ptlmuh006{
         cout << "h " << height << endl;
         cout << "s " << numslices << endl;
         hdr.close();
-        
-        //for each slice
-        //read into slice
-        
-        
         for(int slice = 0; slice < numslices; slice++){
             ostringstream oss;
             oss << baseName << slice << ".raw";
             ifstream slcfile(oss.str(), ios::binary);
             
-            //cout << oss.str() << endl;
-            
-            //cout << "new ";
             unsigned char ** rowsArr = new unsigned char * [height];
             for(int row = 0; row < height; row++){
             
-                //cout << "new ";
                 rowsArr[row] = new unsigned char [width];
                 slcfile.read((char*)rowsArr[row], (sizeof(unsigned char) * width));
                 
             }
-            //cout << endl;
             slcfile.close();
-            slices.push_back(rowsArr);
-            
-            //for(int row = 0; row < height; row++){
-            //    delete[] rowsArr[row];
-            //}
-            //delete [] rowsArr;
+            slices.push_back(rowsArr); 
         }
-        
-        //cout << "loaded: " << slices.size() << endl;
-        //printSlice(slices[3], width, height);
-        //cout << sizeof(unsigned char) << endl;
         
         return true;
     }
     
     void VolImage::extract(int sliceId, std::string output_prefix){
         //write header file for extracted slice
-        ofstream hdr(output_prefix + ".dat");//, ios::trunc);
+        ofstream hdr(output_prefix + ".dat");
         hdr << width << " " << height << " " << 1 << endl;
         hdr.close();
         
         //write extracted slice to output file
-        ofstream out(output_prefix + ".raw", ios::binary); //| ios::trunc);
+        ofstream out(output_prefix + ".raw", ios::binary);
         for(int row = 0; row < height; row++){
             out.write((char *)slices[sliceId][row], (sizeof(unsigned char) * width));
         }
@@ -135,7 +105,7 @@ namespace ptlmuh006{
         ostringstream prefix;
         prefix << output_prefix << "_diff_" << sliceI << "_" << sliceJ;
         
-        ofstream hdr(prefix.str() + ".dat");//, ios::trunc);
+        ofstream hdr(prefix.str() + ".dat");
         hdr << width << " " << height << " " << 1 << endl;
         hdr.close();
         
@@ -147,7 +117,7 @@ namespace ptlmuh006{
             }
         }
         
-        ofstream out(prefix.str() + ".raw", ios::binary); //| ios::trunc);
+        ofstream out(prefix.str() + ".raw", ios::binary);
         for(int row = 0; row < height; row++){
             out.write((char *)diffmapArr[row], (sizeof(unsigned char) * width));
         }
@@ -173,12 +143,12 @@ namespace ptlmuh006{
             currRow++;
         }
         
-        ofstream hdr(prefix.str() + ".dat");//, ios::trunc);
+        ofstream hdr(prefix.str() + ".dat");
         hdr << width << " " << slices.size() << " " << 1 << endl;
         hdr.close();
         
         //write extracted slice to output file
-        ofstream out(prefix.str() + ".raw", ios::binary); //| ios::trunc);
+        ofstream out(prefix.str() + ".raw", ios::binary);
         for(int row = 0; row < slices.size(); row++){
             out.write((char *)extracted[row], (sizeof(unsigned char) * width));
         }
